74ea920fdeb290b0a3b3840dc6baca6f19149d32
THEJAS MONIKUTTAN <115935455+Thejas31@users.noreply.github.com>
2024-06-25 17:33:49 +0530

    Delete IntelProject directory

--- IntelProject/script.js
+++
@@ -1,9 +0,0 @@
-// Get the button
-let topBtn = document.getElementById("topBtn");
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    // Modern browsers
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-}
